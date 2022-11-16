20176b4a24f34a3d657cd60603f17098c995de26
jljiu9 <77003319+jljiu9@users.noreply.github.com>
2022-11-16 12:24:40 +0000

    env

--- database/db.ts
+++ database/db.ts
@@ -1,11 +1,15 @@
 // deno-lint-ignore-file
 import { initializeApp } from "https://esm.sh/firebase/app"
-import { getDatabase } from 'https://esm.sh/firebase/database'
+import { Database, getDatabase } from 'https://esm.sh/firebase/database'
 
-const db = (dbURL: URL | string) => getDatabase(
-    initializeApp({
-        databaseURL: dbURL
-    })
-)
+const db = (dbURL:string|URL|null) => {
+    let url = Deno.env.get('dbURL') 
+    url ? dbURL = url :  1
+    return getDatabase(
+        initializeApp({
+            databaseURL: dbURL
+        })
+    )
+}
 
 export { db }
