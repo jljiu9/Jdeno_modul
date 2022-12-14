fb10f1b5328754dd8eec7d60fca775e98851c5a4
jljiu9 <jljiu@qq.com>
2022-12-14 11:57:29 +0800

    assets

--- database/db.ts
+++ database/db.ts
@@ -1,6 +1,9 @@
 // deno-lint-ignore-file
-import { initializeApp } from "https://esm.sh/firebase/app"
-import { Database, getDatabase } from 'https://esm.sh/firebase/database'
+// import { initializeApp } from "https://esm.sh/firebase/app"
+// import { Database, getDatabase } from 'https://esm.sh/firebase/database'
+//锁定版本
+import { initializeApp } from "https://esm.sh/v98/@firebase/app@0.8.4/es2022/app.js"
+import { Database, getDatabase } from "https://esm.sh/firebase@9.14.0/database"
 
 const db = (dbURL:string|URL|null) => {
     let url = Deno.env.get('dbURL') 
