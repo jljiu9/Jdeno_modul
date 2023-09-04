db7c65dcec59bc6feed99dc7d7a2fe0ef9388a24
jljiu9 <77003319+jljiu9@users.noreply.github.com>
2023-09-04 10:47:36 +0800

    Update db.ts

--- database/db.ts
+++ database/db.ts
@@ -2,8 +2,8 @@
 // import { initializeApp } from "https://esm.sh/firebase/app"
 // import { Database, getDatabase } from 'https://esm.sh/firebase/database'
 //锁定版本
-import { initializeApp } from "https://esm.sh/v98/@firebase/app@0.8.4/es2022/app.js"
-import { Database, getDatabase } from "https://esm.sh/firebase@9.14.0/database"
+import { initializeApp } from "https://esm.sh/firebase@10.3.1/app"
+import { Database, getDatabase } from "https://esm.sh/firebase@10.3.1/database"
 
 const db = (dbURL:string|URL|null) => {
     let url = Deno.env.get('dbURL') 
